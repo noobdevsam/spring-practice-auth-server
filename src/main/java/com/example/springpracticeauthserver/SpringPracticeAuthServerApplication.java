49a3e48aa6f7034c0bd67a4ae50702380369bde6
noobdevsam <mdshoykotislam05@gmail.com>
2025-04-14 01:10:06 +0600

    initial commit

---
+++ src/main/java/com/example/springpracticeauthserver/SpringPracticeAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springpracticeauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPracticeAuthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springpracticeauthserver/SpringPracticeAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springpracticeauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
